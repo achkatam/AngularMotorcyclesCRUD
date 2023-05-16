b8a9df32567042f81b8b5fc592ee5fec44944b38
achkatam <agmatevv@gmail.com>
2023-05-16 12:03:26 -0400

    Motorcycle class encapsulated, added exception messages if incorrect data is applied.

---
+++ MotorcyclesAPI/Common/ExceptionMessages.cs
@@ -0,0 +1,10 @@
+﻿namespace MotorcyclesAPI.Common;
+ 
+public static class ExceptionMessages
+{
+    public const string WRONG_ID = "Id cannot be 0 or less.";
+    public const string WRONG_MAKE = "Make cannot be null or empty.";
+    public const string WRONG_MODEL = "Model cannot be null or empty.";
+    public const string WRONG_YEAR = $"Year cannot be less than 1900.";
+    public const string WRONG_PRICE = "Price cannot be 0 or less.";
+}
--- MotorcyclesAPI/Models/Motorcycle.cs
+++ MotorcyclesAPI/Models/Motorcycle.cs
@@ -1,11 +1,90 @@
 ﻿namespace MotorcyclesAPI.Models;
 
+using Common;
+
 public class Motorcycle
 {
-    public int Id { get; set; }
-    public string Make { get; set; } = string.Empty;
-    public string Model { get; set; } = string.Empty;
-    public string Year { get; set; } = string.Empty;
+    private readonly int _id;
+    private readonly string _model;
+    private readonly string _make;
+    private readonly string _year;
+    private decimal _price;
 
-    public decimal Price { get; set; }
+    public Motorcycle(string model, string make, string year, decimal price)
+    {
+        Model = model;
+        Make = make;
+        Year = year;
+        Price = price;
+    }
+
+    public int Id
+    {
+        get { return _id; }
+        private init
+        {
+            if (value <= 0)
+            {
+                throw new ArgumentException(ExceptionMessages.WRONG_ID);
+            }
+
+            _id = value;
+        }
+    }
+
+    public string Make
+    {
+        get { return _make; }
+        private init
+        {
+            if (string.IsNullOrWhiteSpace(value))
+            {
+                throw new ArgumentException(ExceptionMessages.WRONG_MAKE);
+            }
+
+            _make = value;
+        }
+    }
+
+    public string Model
+    {
+        get { return _model; }
+        private init
+        {
+            if (string.IsNullOrWhiteSpace(value))
+            {
+                throw new ArgumentException(ExceptionMessages.WRONG_MODEL);
+            }
+
+            _model = value;
+        }
+    }
+
+    public string Year
+    {
+        get { return _year; }
+        private init
+        {
+            if (string.IsNullOrWhiteSpace(value) || int.Parse(value) <= 1900)
+            {
+                throw new ArgumentException(ExceptionMessages.WRONG_YEAR);
+            }
+
+            _year = value;
+        }
+    }
+
+    public decimal Price
+    {
+        get { return _price; }
+        internal set
+        {
+            if (value <= 0)
+            {
+                throw new ArgumentException(ExceptionMessages.WRONG_PRICE);
+            }
+
+            _price = value;
+        }
+    }
 }
